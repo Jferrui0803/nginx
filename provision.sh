0fd92e481993fc5546c7c9fe1fb18b9985ff213e
José Manuel Fernández Ruiz <jferrui0803@ieszaidinvergeles.org>
2024-11-12 18:39:39 +0100

    actualizo provision.sh

--- provision.sh
+++ provision.sh
@@ -15,22 +15,12 @@ git clone https://github.com/cloudacademy/static-website-example /var/www/mipagw
 
 # Asignar permisos adecuados
 sudo chown -R www-data:www-data /var/www/mipagweb/html
-sudo chmod -R 755 /var/www/mipagweb
+sudo chmod -R 755 /var/www/mipagweb/html
+
 
 # 3. Configurar Nginx para servir el sitio web
 # Crear archivo de configuración del sitio en sites-available
-sudo bash -c 'cat > /etc/nginx/sites-available/mipagweb <<EOF
-server {
-    listen 80;
-    listen [::]:80;
-    root /var/www/mipagweb/html;
-    index index.html index.htm index.nginx-debian.html;
-    server_name www.jferrui.test;
-    location / {
-        try_files \$uri \$uri/ =404;
-    }
-}
-EOF'
+sudo cp /vagrant/mipagweb /etc/nginx/sites-available/
 
 # Crear enlace simbólico en sites-enabled
 sudo ln -s /etc/nginx/sites-available/mipagweb /etc/nginx/sites-enabled/
@@ -40,28 +30,41 @@ sudo adduser jferrui
 echo "jferrui:jferrui" | sudo chpasswd
 
 # Crea la carpeta (aunque debería estar creada al crear el usuario)
-sudo mkdir /home/jferrui/ftp
-
-# Permisos para la carpeta
-sudo chown vagrant:vagrant /home/jferrui/ftp
+sudo mkdir -p /home/jferrui/ftp
+sudo chown jferrui:jferrui /home/jferrui/ftp
 sudo chmod 755 /home/jferrui/ftp
 
 # Crear los certificados de seguridad
-sudo openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /etc/ssl/private/vsftpd.key -out /etc/ssl/certs/vsftpd.crt
+sudo openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /etc/ssl/private/vsftpd.key -out /etc/ssl/certs/vsftpd.crt -subj "/C=ES/ST=Granada/L=Granada/O=Global Security/OU=IES ZAIDIN VERGELES/CN=extreme-sports.test"
+
+# Subir el archivo de configuración de vsftpd
+sudo cp -v /vagrant/vsftpd.conf /etc/vsftpd.conf
+
+# Reiniciar el servicio de vsftpd
+sudo systemctl start  vsftpd
 
 # Agregar el usuario jferrui al grupo www-data
 sudo usermod -aG www-data jferrui
 
 # Crear la nueva carpeta de la página web
-sudo mkdir -p /var/www/Extreme-Sports
+sudo mkdir -p /var/www/Extreme-Sports/html/
 
 # Asignar permisos
-sudo chown www-data:www-data /var/www/Extreme-Sports
-sudo chmod 775 /var/www/Extreme-Sports
+sudo chown -R www-data:www-data /var/www    #dar permisos a /var/www para hacer la transferecia en Filezilla
+sudo chmod -R 755 /var/www/Extreme-Sports/
+
+# Copiar archivo de configuración desde /vagrant a /etc/nginx/sites-available
+sudo cp /vagrant/Extreme-Sports /etc/nginx/sites-available/
 
 # Crear enlace simbólico en sites-enabled
 sudo ln -s /etc/nginx/sites-available/Extreme-Sports /etc/nginx/sites-enabled/
 
+# Verificar la configuración de Nginx antes de reiniciar
+sudo nginx -t
+
 # Reiniciar Nginx para aplicar los cambios
 sudo systemctl restart nginx
-# Si da un error de 404 FORBIDDEN ejecutar el siguiente comando
+
+# Verificar que Nginx esté funcionando
+sudo systemctl status nginx
+
